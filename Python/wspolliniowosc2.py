761b556cced03b7132b21a2fac20bbd7f45d7d6c
Programista3 <programista333@gmail.com>
2018-01-07 19:01:24 +0100

    Przystosowanie programów do Pythona 3.6

--- Python/wspolliniowosc2.py
+++ Python/wspolliniowosc2.py
@@ -8,12 +8,12 @@ def det(a, b, c):
 	return (a[0]*b[1]*c[2]+a[1]*b[2]*c[0]+a[2]*b[0]*c[1])-(a[2]*b[1]*c[0]+a[0]*b[2]*c[1]+a[1]*b[0]*c[2])
 
 print(u"Podaj współrzędne punktu a:")
-a = raw_input()
+a = input()
 print(u"Podaj współrzędne punktu b:")
-b = raw_input()
+b = input()
 print(u"Podaj współrzędne punktu c:")
-c = raw_input()
-det = det(map(int, a.split(',')), map(int, b.split(',')), map(int, c.split(',')))
+c = input()
+det = det(list(map(int, a.split(','))), list(map(int, b.split(','))), list(map(int, c.split(','))))
 print('det(a,b,c) = '+str(det))
 if det == 0:
 	print(u"Punkty a, b, c są współliniowe")
