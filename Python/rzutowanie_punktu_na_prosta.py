787743ace2c3ba5ffaf672191e562f8679732b4b
Programista3 <programista333@gmail.com>
2018-07-25 22:14:07 +0200

    changed structure of files and directories

--- Python/rzutowanie_punktu_na_prosta.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Rzutowanie punktu na prostą
-
-import numpy as np
-
-def projection(p, p1, p2):
-    u = float(np.dot(np.subtract(p, p1).tolist(), np.subtract(p2, p1).tolist()))/float(np.dot(np.subtract(p1, p2).tolist(), np.subtract(p1, p2).tolist()))
-    return np.add(p1, np.multiply(np.subtract(p2, p1).tolist(), u).tolist()).tolist()
