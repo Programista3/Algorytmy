978fd0fb5c863e1144adefdb32cdf8773a10587d
Programista3 <programista333@gmail.com>
2018-08-02 14:58:16 +0200

    Added keyboard events, support for multi-digit numbers and fractions

--- pyCalc3/pyCalc.py
+++ pyCalc3/pyCalc.py
@@ -21,9 +21,8 @@ class Stack:
     def get(self):
         return self.stack[::-1]
 
-
 class pyCalc:
-    version = "3.0.4"
+    version = "3.0.5"
 
     def __init__(self):
         self.startGui()
@@ -41,6 +40,10 @@ class pyCalc:
         self.frame = Frame(self.root)
         self.frame.grid(row=0, column=0, rowspan=len(btnLabels)+1, columnspan=len(btnLabels[0]))
         self.textbox = ttk.Entry(self.frame, font=('Helvetica', '16'), justify=RIGHT)
+        self.root.bind("<Return>", lambda event: self.click("="))
+        self.root.bind("<BackSpace>", lambda event: self.click("AC"))
+        self.root.bind("<Delete>", lambda event: self.click("C"))
+        self.root.bind("<Key>", lambda event: self.click(event.char))
         self.textbox.grid(row=0, columnspan=len(btnLabels[0]), sticky=W+E, padx=3, pady=3)
         self.btn = []
         for r in range(1,len(btnLabels)+1):
@@ -52,7 +55,9 @@ class pyCalc:
 
     def click(self, btn):
         if(btn == '='):
-            print(self.calculateRPN(self.convertToRPN(self.textbox.get())))
+            result = self.calculateRPN(self.convertToRPN(self.textbox.get()))
+            self.textbox.delete(0, END)
+            self.textbox.insert(END, result)
         elif(btn == 'C'):
             self.textbox.delete(0, END)
         elif(btn == 'AC'):
@@ -62,6 +67,16 @@ class pyCalc:
 
     def isOperator(self, char):
         return char in ['+','-','*','/','^']
+
+    def isNumner(self, str):
+        if(str.isdigit()):
+            return 1
+        else:
+            try:
+                float(str)
+                return 2
+            except ValueError:
+                return 0
 
     def priority(self, operator):
         if(operator == '+' or operator == '-'):
@@ -74,21 +89,27 @@ class pyCalc:
             return 0
 
     def convertToRPN(self, text):
+        text = text.replace(',', '.')
         stack = Stack()
         output = []
-        for c in text:
-            if(c.isdigit()):
-                output.append(c)
-            elif(self.isOperator(c)):
+        for i in range(len(text)):
+            if(text[i].isdigit()):
+                if(i > 0) and (text[i-1].isdigit() or text[i-1] == '.'):
+                    output[len(output)-1] += text[i]
+                else:
+                    output.append(text[i])
+            elif(text[i] == '.'):
+                output[len(output)-1] += text[i]
+            elif(self.isOperator(text[i])):
                 for i in range(stack.size()):
-                    if(self.priority(c) == 3 or self.priority(c) > self.priority(stack.top())):
+                    if(self.priority(text[i]) == 3 or self.priority(text[i]) > self.priority(stack.top())):
                         break
                     output.append(stack.top())
                     stack.pop()
-                stack.push(c)
-            elif(c == '('):
-                stack.push(c)
-            elif(c == ')'):
+                stack.push(text[i])
+            elif(text[i] == '('):
+                stack.push(text[i])
+            elif(text[i] == ')'):
                 while stack.top() != '(':
                     output.append(stack.top())
                     stack.pop()
@@ -100,8 +121,10 @@ class pyCalc:
         stack = Stack()
         output = []
         for c in rpn:
-            if(c.isdigit()):
-                stack.push(c)
+            if(self.isNumner(c) == 1):
+                stack.push(int(c))
+            elif(self.isNumner(c) == 2):
+                stack.push(float(c))
             elif(self.isOperator(c)):
                 a = stack.top()
                 stack.pop()
